a79a08c2b1f59240ee69a5318dc82be2cb86bc4c
PhilSchoeb <philippe.schoeb@gmail.com>
2024-12-07 11:39:22 -0500

    restruture the folder ift6758

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ift6758',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project repo for Milestone-3 IFT6758-2024 Project for group A-05',
+    author='Philippe Schoeb, Ilias Zouine, Thibault Maumy, Edouard Demotes-Mainard',
+    license='',
+)
