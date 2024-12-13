5cac8872e2bed27067f2f553ca483240014b5f81
Edouard Demotes <e.demotes@gmail.com>
2024-12-12 23:22:37 -0500

    chore: improve run script

--- run.sh
+++ run.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-# Environment variables
-PORT="8000"
-WANDB_API_KEY="YOUR_WANDB_API_KEY"
-WANDB_ORG="IFT6758-2024-A05"
-
-# Image tag
-TAG="latest"
-
 # Exit on error
 set -e
 
+# Container options
+PORT="8000"
+TAG="latest"
+NAME="ift6758-serving"
+
 # Colors for the terminal
 YELLOW='\033[1;33m'
+PURPLE='\033[0;35m'
 GREEN='\033[0;32m'
 NO_COLOR='\033[0m'
 
+# Load .env file if it exists
+# Allows to set the WANDB_API_KEY and WANDB_ORG without exposing them in the script
+if [ -f .env ] ; then
+  echo -e "${PURPLE}Loading environment variables from .env file${NO_COLOR}"
+  export $(cat .env | xargs)
+fi
+
 echo -e "${YELLOW}Stop previous docker images (if any)${NO_COLOR}"
-docker stop ift6758-serving || true
+docker stop ${NAME} || true
 
 echo -e "${YELLOW}Run the docker images${NO_COLOR}"
 docker run \
@@ -26,7 +31,7 @@ docker run \
   -p ${PORT}:8000 \
   -e WANDB_API_KEY=${WANDB_API_KEY} \
   -e WANDB_ORG=${WANDB_ORG} \
-  --name ift6758-serving \
+  --name ${NAME} \
   ift6758/serving:${TAG}
 
 echo -e "${GREEN}Serving the model at http://localhost:${PORT}${NO_COLOR}"
